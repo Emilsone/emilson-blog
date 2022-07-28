23a552e122ec98e7764497f5a837d36d9d9ee536
Emilsone <lawsonezekiel68@gmail.com>
2022-07-28 21:56:00 +0100

    Add article category

--- pages/index.vue
+++ pages/index.vue
@@ -109,7 +109,7 @@
           <div class="p-4 lg:w-1/3">
             <div
               class="
-                h-80
+                h-96
                 px-8
                 pt-16
                 pb-24
@@ -129,7 +129,7 @@
                   mb-1
                 "
               >
-                CATEGORY
+                Javascipt | Currying 
               </h2>
               <h1
                 class="
@@ -168,7 +168,7 @@
           <div class="p-4 lg:w-1/3">
             <div
               class="
-                h-80
+                h-96
                 px-8
                 pt-16
                 pb-24
@@ -188,7 +188,7 @@
                   mb-1
                 "
               >
-                CATEGORY
+                VueJs | State | Vue.observable
               </h2>
               <h1
                 class="
@@ -242,7 +242,7 @@
           <div class="p-4 lg:w-1/3">
             <div
               class="
-                h-80
+                h-96
                 px-8
                 pt-16
                 pb-24
@@ -262,7 +262,7 @@
                   mb-1
                 "
               >
-                CATEGORY
+                Javascipt | Programming
               </h2>
               <h1
                 class="
@@ -275,6 +275,216 @@
                 "
               >
                 Programming - Imperative vs Declarative
+              </h1>
+              <p class="leading-relaxed mb-3 text-white">
+                In this post, we’ll go over what imperative and declarative in
+                programming are, and the differences between them, with a code
+                example.
+              </p>
+              <a class="text-indigo-500 inline-flex items-center"
+                >Read More
+                <svg
+                  class="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              <div
+                class="
+                  text-center
+                  mt-2
+                  leading-none
+                  flex
+                  justify-center
+                  absolute
+                  bottom-0
+                  left-0
+                  w-full
+                  py-4
+                "
+              ></div>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex flex-wrap -m-4">
+          <div class="p-4 lg:w-1/3">
+            <div
+              class="
+                h-96
+                px-8
+                pt-16
+                pb-24
+                rounded-sm
+                overflow-hidden
+                relative
+                article-card
+              "
+            >
+              <h2
+                class="
+                  tracking-widest
+                  text-xs
+                  title-font
+                  font-medium
+                  text-gray-400
+                  mb-1
+                "
+              >
+                State | Javascipt
+              </h2>
+              <h1
+                class="
+                  title-font
+                  sm:text-2xl
+                  text-xl
+                  font-medium
+                  text-white
+                  mb-3
+                "
+              >
+                Managing your State Application in Javascript
+              </h1>
+              <p class="leading-relaxed mb-3 text-white">
+                Redux, Vuex, and other technologies come to mind when we hear
+                the phrase state in Javascript. But in this article, I'm going
+                to give an in-depth explanation of what state is and how to
+                manage your state application in pure Javascript.
+              </p>
+              <a class="text-indigo-500 inline-flex items-center">Read More </a>
+              <div
+                class="
+                  text-center
+                  mt-2
+                  leading-none
+                  flex
+                  justify-center
+                  absolute
+                  bottom-0
+                  left-0
+                  w-full
+                  py-4
+                "
+              ></div>
+            </div>
+          </div>
+          <div class="p-4 lg:w-1/3">
+            <div
+              class="
+                h-96
+                px-8
+                pt-16
+                pb-24
+                rounded-sm
+                overflow-hidden
+                relative
+                article-card
+              "
+            >
+              <h2
+                class="
+                  tracking-widest
+                  text-xs
+                  title-font
+                  font-medium
+                  text-gray-400
+                  mb-1
+                "
+              >
+                Vue | Component | Keep-alive
+              </h2>
+              <h1
+                class="
+                  title-font
+                  sm:text-2xl
+                  text-xl
+                  font-medium
+                  text-white
+                  mb-3
+                "
+              >
+                Caching your Dynamic components with Vue Keep-Alive
+              </h1>
+              <p class="leading-relaxed mb-3 text-white">
+                In this article, we’ll explain what Vue.observable is, and how
+                we can use it to manage our simple store across our application
+                in Vue.js.
+              </p>
+              <a class="text-indigo-500 inline-flex items-center"
+                >Read More
+                <svg
+                  class="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              <div
+                class="
+                  text-center
+                  mt-2
+                  leading-none
+                  flex
+                  justify-center
+                  absolute
+                  bottom-0
+                  left-0
+                  w-full
+                  py-4
+                "
+              ></div>
+            </div>
+          </div>
+
+          <div class="p-4 lg:w-1/3">
+            <div
+              class="
+                h-96
+                px-8
+                pt-16
+                pb-24
+                rounded-sm
+                overflow-hidden
+                relative
+                article-card
+              "
+            >
+              <h2
+                class="
+                  tracking-widest
+                  text-xs
+                  title-font
+                  font-medium
+                  text-gray-400
+                  mb-1
+                "
+              >
+                VueJS
+              </h2>
+              <h1
+                class="
+                  title-font
+                  sm:text-2xl
+                  text-xl
+                  font-medium
+                  text-white
+                  mb-3
+                "
+              >
+                Best Practices for Writing Vue.js
               </h1>
               <p class="leading-relaxed mb-3 text-white">
                 In this post, we’ll go over what imperative and declarative in
